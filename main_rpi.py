9845a21731cc73966ed9e120d861bfa3697ce5fd
maximmat <maximmatous@seznam.cz>
2024-05-08 20:35:35 +0200

    Post na LOOPBACK, Vycitani informaci k udalsoti

--- main_rpi.py
+++ main_rpi.py
@@ -1,13 +1,12 @@
 import requests
 from telethon import TelegramClient, events
 
-url = "http://192.168.1.7:1880/poplach" #endpoint v nodered
-data = {"fireport": "poplach"} #data co dorazí do noderedu
+url = "http://127.0.0.1:1880/poplach" #loopback na endpoint v nodered
 
 #data z api
-api_id = "12345"
-api_hash = "b123548a2356452f1544"
-sesion_name = "Fireport"
+api_id = "api_id"
+api_hash = "api_hash"
+sesion_name = "sesion_name"
 #----
 
 chat = "FIREPORTcz_bot" #jméno fireportu na telegramu
@@ -15,12 +14,25 @@ chat = "FIREPORTcz_bot" #jméno fireportu na telegramu
 
 client = TelegramClient(sesion_name, api_id, api_hash) #definice klienta z knihovny telethon viz. 2 řádek
 
-@client.on(events.NewMessage(chats=chat)) #na co má klient reagovat
+@client.on(events.NewMessage(chats=chat, incoming=True)) #na co má klient reagovat
 async def newmess(event): #kdž přijde nová zpráva, async ve zkratce říká, že se funkce provádějí nezávisle na sobě, pokud by bylo více funkcí nečeká se než se dokončí jedna, ale zároveň se vykonává druhá (https://realpython.com/python-async-features/)
-        response = requests.post(url, json=data) #posílání přes post na nodered
+	mess = event.raw_text.splitlines() #vyčtení zprávy a rozdělení řádků do seznamu
 
-        print(response) #výpis odpovedi od noderedu, není potřeba je pro ujištění se, že data došli
-        #v nodered je potřeba navázat http response s kédem 200, aby python skript veděl, že nodered data přijal
+	kategorie = mess[0] #ze sezanmu jeddnotlivé informace
+	lokace = mess[1]
+	dopres = mess[2]
+	tech = mess[3]
+
+	kategorie = kategorie[2:] #odebrání emoji ze zprávy
+	lokace = lokace[3:] #emoji pro lokaci a dopřeasnění zabírají dva charaktery
+	dopres = dopres[3:]
+	tech = tech[2:]
+
+	data = {"fireport": "poplach", "kategorie": kategorie, "lokace": lokace, "dopres": dopres, "tech": tech} # data pro nodered
+	response = requests.post(url, json=data) #posílání přes post na nodered
+
+	print(response) #výpis odpovedi od noderedu, není potřeba je pro ujištění se, že data došli
+	#v nodered je potřeba navázat http response s kódem 200, aby python skript veděl, že nodered data přijal
 
 client.start() #start telegram klienta
 client.run_until_disconnected() #nekonečná smyčka pro telegram klienta
